ef43cd264dd56b774820865b4c2449888b32974a
scotthavird <scott.havird@icloud.com>
2023-09-07 14:33:17 -0400

    adds documentation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Open AI Whisper
 Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse audio and is also a multi-task model that can perform multilingual speech recognition as well as speech translation and language identification. https://github.com/openai/whisper
 
+https://github.com/openai/whisper/discussions/63
+https://prameshbajra.github.io/ai/code/aws/docker/lambda/containers/ffmpeg/2023/05/22/deploy-lambda-container.html
+
+
 ## pre-requisite
 
 - [install docker](https://www.docker.com/get-started/)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import whisper
 import json
+import os
 
 def main():    
     print("loading model...")    
     model = whisper.load_model("base")
 
     print("predicting...")
-    #result = model.transcribe("861b9e79-dbac-4a63-bfb7-c06e7f82e1c3.mp3")
-    result = model.transcribe("https://augie-public-test.s3.amazonaws.com/27a8d8a9-5624-4b2f-a425-4fb99a891779/ff23affc-9ab7-458f-9792-d7b5a19ef223/861b9e79-dbac-4a63-bfb7-c06e7f82e1c3.mp3")    
+    audio_url = os.environ.get("AUDIO_URL")
+    
+    result = model.transcribe(
+        audio_url, word_timestamps=True
+    )
 
-    json_object = json.dumps(result, indent=4)
+    data = json.dumps(result, indent=4)
     
     # write to disk
     with open("data.json", "w") as outfile:
-        outfile.write(json_object)
+        outfile.write(data)
     
 if __name__ == "__main__":
     main()
